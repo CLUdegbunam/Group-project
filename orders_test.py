d46b088c3fd2a0e38057ffac3940338eedcd24f4
KhalidS <janjan1000@live.com>
2022-01-28 13:34:28 +0000

    Updaes

--- orders_test.py
+++ orders_test.py
@@ -41,9 +41,6 @@ with open("chesterfield_25-08-2021_09-00-00.csv", 'r') as chesterfield_cafe_orde
             orders.append({"Date_Time" : line[0], "Branch" : line[1], "Total_Price" : line[4]})
             items_ordered.append({"Item_Name" : product})
 
-# for i in orders:
-#     print(i["Date_Time"])
-
 
 load_dotenv()
 host = os.environ.get("pg_host")
@@ -89,7 +86,7 @@ def update_db():
         VALUES (
         DEFAULT,
         '{i['Date_Time']}',
-        'Chesterfield',
+        1,
         {i['Total_Price']}
         )
         """
@@ -112,41 +109,49 @@ def abc():
         run_db(sql)
         # '{i['Item_Name']}'
 
+def display_orders():
+    sql = """
+    SELECT o.Order_ID, b.Branch, o.Date_Time, o.Total_Price
+    FROM Branches b
+    INNER JOIN Orders o ON o.Branch_ID = b.Branch_ID
+    """
+    run_db(sql)
 
-    # SELECT o.Order_ID, b.Branch, o.Date_Time, o.Total_Price
-    # FROM Branches b
-    # INNER JOIN Orders o ON o.Branch_ID = b.Branch_ID
+option = input("""
+Enter '1' to create an All_Orders database,
+Enter '2' to create an Orders table,
+Enter '3' to add the orders to the table: 
+Enter '4' to display all the orders: 
+""")
 
+while True:
+    try:
+        if option == "":
+            option = input("""
+Enter '1' to create a Cafe_Orders database,
+Enter '2' to create an Orders table,
+Enter '3' to add the orders to the table,
+Enter '4' to display all the orders: 
+""")
 
-# option = input("""
-# Enter '1' to create an All_Orders database,
-# Enter '2' to create an Orders table,
-# Enter '3' to add the orders to the table: 
-# """)
+        elif option == '1':
+            sql = create_db()
+            run_db(sql)
+            break
+        elif option == '2':
+            sql = create_tables()
+            run_db(sql)
+            break
+        elif option == '3':
+            update_db()
+            # abc()
+            break
+        elif option == '4':
+            display_orders()
+            break
 
-# while True:
-#     try:
-#         if option == "":
-#             option = input("""
-# Enter '1' to create an All_Orders database,
-# Enter '2' to create an Orders table,
-# Enter '3' to add the orders to the table: 
-#             """)
+    except Exception as e:
+        print("Exception: ",e)
+        break
 
-#         elif option == '1':
-#             sql = create_db()
-#             run_db(sql)
-#             break
-#         elif option == '2':
-#             sql = create_tables()
-#             run_db(sql)
-#             break
-#         elif option == '3':
-#             abc()
-#             break
-
-#     except Exception as e:
-#         print("Exception: ",e)
-#         break
-
-load_id()
+# load_id()
