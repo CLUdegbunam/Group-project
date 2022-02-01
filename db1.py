62ca45109f6218c2556b96bbf0a16d8906cf7cd9
KhalidS <janjan1000@live.com>
2022-02-01 10:56:32 +0000

    Updates

--- db1.py
+++ db1.py
@@ -1,14 +1,10 @@
-
-def create_db():
-    sql = "CREATE DATABASE Cafe_Orders";
-    return sql
-
 def create_tables():
     sql = """  
     CREATE TABLE IF NOT EXISTS Item (
         Item_ID SMALLSERIAL PRIMARY KEY,
         Item_Name VARCHAR(255) NOT NULL,
-        Price FLOAT(2) NOT NULL
+        Price FLOAT(2) NOT NULL,
+        UNIQUE (Item_Name)
     );
 
     CREATE TABLE IF NOT EXISTS Orders(
@@ -20,49 +16,39 @@ def create_tables():
     );
     CREATE TABLE IF NOT EXISTS Branches (
         Branch_ID SMALLSERIAL PRIMARY KEY,
-        Branch VARCHAR(255) NOT NULL
+        Branch VARCHAR(255) NOT NULL,
+        UNIQUE (Branch)
     );
     CREATE TABLE IF NOT EXISTS Items_Ordered (
-        Order_ID INT NOT NULL,
+        Order_ID SMALLSERIAL,
         Item_ID INT NOT NULL,
-        Quantity INT NOT NULL,
         PRIMARY KEY (Order_ID, Item_ID)
     );
-
-
-
-    INSERT INTO Item (
-        Item_ID, Item_Name, Price
-        )
-        VALUES
-        (DEFAULT, 'Regular Flavoured iced latte - Hazelnut', 2.75),
-        (DEFAULT, 'Regular Flavoured iced latte - Caramel', 2.75),
-        (DEFAULT, 'Regular Flavoured iced latte - Vanilla', 2.75),
-        (DEFAULT, 'Large Flavoured iced latte - Hazelnut', 3.25),
-        (DEFAULT, 'Large Flavoured iced latte - Caramel', 3.25),
-        (DEFAULT, 'Large Flavoured iced latte - Vanilla', 3.25),
-        (DEFAULT, 'Large Flat white', 2.45),
-        (DEFAULT, 'Regular Flavoured latte - Hazelnut', 2.55),
-        (DEFAULT, 'Regular Flavoured latte - Caramel', 2.55),
-        (DEFAULT, 'Regular Flavoured latte - Vanilla', 2.55),
-        (DEFAULT, 'Regular Latte', 2.15),
-        (DEFAULT, 'Large Flavoured latte - Hazelnut', 2.85),
-        (DEFAULT, 'Large Flavoured latte - Caramel', 2.85),
-        (DEFAULT, 'Large Flavoured latte - Vanilla', 2.85),
-        (DEFAULT, 'Regular Flat white', 2.15),
-        (DEFAULT, 'Large Latte', 2.45);
-
-
-    INSERT INTO Branches(
-        Branch_ID, Branch
-        )
-        VALUES
-        (DEFAULT, 'Chesterfield'),
-        (DEFAULT, 'Kensington')
     """
     return sql
-    
-# USE Cafe_Orders
-# SELECT Orders.Branch_ID, Branches.Branch
-# FROM Branches
-# INNER JOIN Orders ON Orders.Branch_ID = Branches.Branch_ID
+
+def insert_column_values():    
+            from orders_test import products123, price_for_product, run_db, Branchess
+            for prices, item in enumerate(products123):
+                price = price_for_product[prices]
+                price = float(price)
+
+                sql = F"""
+                INSERT INTO Item 
+                VALUES (
+                DEFAULT, '{item}', {price}
+                )
+                ON CONFLICT DO NOTHING
+                """
+                run_db(sql)
+
+            for Branch in Branchess:
+                sql = f"""
+                INSERT INTO Branches(
+                Branch_ID, Branch
+                )
+                VALUES
+                (DEFAULT, '{Branch}')
+                ON CONFLICT DO NOTHING
+            """
+                run_db(sql)
--- orders_test.py
+++ orders_test.py
@@ -1,7 +1,8 @@
 import csv
+from operator import index
 import os
 import psycopg2
-from db1 import create_db, create_tables
+from db1 import create_tables, insert_column_values
 from dotenv import load_dotenv
 
 
@@ -11,36 +12,10 @@ items_ordered = []
 products = []
 final_products = []
 
+Branchess = []
 
-with open("chesterfield_25-08-2021_09-00-00.csv", 'r') as chesterfield_cafe_orders:
-    reader = csv.reader(chesterfield_cafe_orders)
-
-    for line in reader:
-        final_products = []
-        test_for_products = []
-        test_for_products = line[3]
-        if ', ' in test_for_products:
-            test_for_products = line[3].split(', ')
-
-        x = 0
-        for product in test_for_products:
-            if '- ' in product:
-                x =+1
-                products = product.rsplit(' - ', 1)
-                products.remove(products[-1])
-                final_products.append(products[0])
-
-        if x == 0:
-            if '- ' in test_for_products:
-                products = test_for_products.rsplit(' - ', 1)
-                products.remove(products[-1])
-                final_products.append(products[0])
-
-            
-        for product in final_products:
-            orders.append({"Date_Time" : line[0], "Branch" : line[1], "Total_Price" : line[4]})
-            items_ordered.append({"Item_Name" : product})
-
+products123 = []
+price_for_product = []
 
 load_dotenv()
 host = os.environ.get("pg_host")
@@ -60,9 +35,24 @@ def run_db(sql):
         connection.autocommit=True
         cursor = connection.cursor()
         cursor.execute(sql)
+
+        cursor.close()
+    finally:
+        connection.close()
+    
+
+def run_db_with_return(sql):
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database,
+        )
+        connection.autocommit=True
+        cursor = connection.cursor()
+        cursor.execute(sql)
         rows = cursor.fetchall()
-
-
 
         cursor.close()
     finally:
@@ -72,12 +62,62 @@ def run_db(sql):
 
 
 def load_id():
-    for i in orders:
-        sql = f"""
-        SELECT Branch_ID {i['Branch']}
-        FROM Branches
-        """
-        run_db(sql)
+    try:
+        sql = f"SELECT Branch FROM Branches"
+        a = run_db_with_return(sql)
+        for branch in a:
+            if branch[0] not in Branchess:
+                Branchess.append(branch[0])
+    except Exception as e:
+        pass
+
+load_id()
+
+
+with open("chesterfield_25-08-2021_09-00-00.csv", 'r') as chesterfield_cafe_orders:
+    reader = csv.reader(chesterfield_cafe_orders)
+
+    for line in reader:
+        final_products = []
+        test_for_products = []
+
+        test_for_products = line[3]
+        if ', ' in test_for_products:
+            test_for_products = line[3].split(', ')
+
+        x = 0
+        for product in test_for_products:
+            pricess = []
+            if '- ' in product:
+                x =+ 1
+                products = product.rsplit(' - ', 1)
+                pricess.append(product.split(' - ', -2))
+                products.remove(products[-1])
+
+                if products[0] not in products123:
+                    products123.append(products[0])
+                    price_for_product.append(pricess[0][-1])
+
+                final_products.append(products[0])
+
+        if x == 0:
+            if '- ' in test_for_products:
+                products = test_for_products.rsplit(' - ', 1)
+                pricess.append(products[1])
+                products.remove(products[-1])
+
+                if products[0] not in products123:
+                    products123.append(products[0])
+                    price_for_product.append(pricess[0])
+
+                final_products.append(products[0])
+
+        if line[1] not in Branchess:
+            Branchess.append(line[1])
+
+        for product in final_products:
+            orders.append({"Date_Time" : line[0], "Branch" : Branchess.index(line[1])+1, "Total_Price" : line[4]})
+            items_ordered.append({"Item_Name" : (products123.index(product)+1)})
 
 
 def update_db():
@@ -86,77 +126,57 @@ def update_db():
         sql = f"""
         SET datestyle = dmy;
         INSERT INTO Orders (
-            Order_ID, Date_Time, Branch_ID, Total_Price
-        )
-        VALUES (
-        DEFAULT,
-        '{i['Date_Time']}',
-        1,
-        {i['Total_Price']}
-        )
+        Order_ID, Date_Time, Branch_ID, Total_Price)
+        VALUES (DEFAULT, '{i['Date_Time']}',
+        {i["Branch"]}, {i['Total_Price']})
         """
         run_db(sql)
-
-    
-
-def abc():    
+        
     for i in items_ordered:
         sql = f"""
         INSERT INTO Items_Ordered (
-            Order_ID, Item_ID, Quantity
-        )
-        VALUES (
-            DEFAULT,
-            1,
-            2
-        )
+        Order_ID, Item_ID)
+        VALUES (DEFAULT, {i['Item_Name']})
         """
         run_db(sql)
-        # '{i['Item_Name']}'
 
-def display_orders():
+
+def display_all_orders():
     sql = """
-    SELECT o.Order_ID, b.Branch, o.Date_Time, o.Total_Price
-    FROM Branches b
-    INNER JOIN Orders o ON o.Branch_ID = b.Branch_ID
-    """
-    print(run_db(sql))
+    SELECT o.Order_ID, b.Branch, to_char(o.Date_Time, 'DD-MM-YYYY HH:MI'), it.Item_Name, o.Total_Price
+    FROM Orders o
+    INNER JOIN Branches b ON b.Branch_ID = o.Branch_ID
+    INNER JOIN Items_Ordered io ON o.Order_ID = io.Order_ID
+    INNER JOIN Item it ON it.Item_ID = io.Item_ID
 
-option = input("""
-Enter '1' to create an All_Orders database,
-Enter '2' to create an Orders table,
-Enter '3' to add the orders to the table: 
-Enter '4' to display all the orders: 
-""")
+    """
+    orders = (run_db_with_return(sql))
+    for order in orders:
+        print(order)
+
 
 while True:
-    try:
-        if option == "":
-            option = input("""
-Enter '1' to create a Cafe_Orders database,
-Enter '2' to create an Orders table,
-Enter '3' to add the orders to the table,
-Enter '4' to display all the orders: 
+    option = input("""
+Enter '1' to create all the tables,
+Enter '2' to add the columns to the tables
+Enter '3' to add the orders to the tables 
+Enter '4' to display all the orders
+Enter '0' to exit application: 
 """)
-
-        elif option == '1':
-            sql = create_db()
-            run_db(sql)
-            break
-        elif option == '2':
+    try:
+        if option == '1':
             sql = create_tables()
             run_db(sql)
-            break
-        elif option == '3':
+        elif option == '2':
+            sql = insert_column_values()
+            run_db(sql)
+        elif option == '3':  
             update_db()
-            # abc()
-            break
         elif option == '4':
-            display_orders()
+            display_all_orders()
+        elif option == '0':
             break
 
     except Exception as e:
         print("Exception: ",e)
         break
-
-# load_id()
