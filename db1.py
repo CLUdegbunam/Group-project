8d04ef1eea85c3d53f747e075994132a1b096eb3
KhalidS <janjan1000@live.com>
2022-02-03 15:19:15 +0000

    The updates you asked for :)

--- db1.py
+++ db1.py
@@ -1,6 +1,6 @@
 def create_tables():
     sql = """  
-    CREATE TABLE IF NOT EXISTS Item (
+    CREATE TABLE IF NOT EXISTS Items (
         Item_ID SMALLSERIAL PRIMARY KEY,
         Item_Name VARCHAR(255) NOT NULL,
         Price FLOAT(2) NOT NULL,
@@ -8,11 +8,11 @@ def create_tables():
     );
 
     CREATE TABLE IF NOT EXISTS Orders(
-        Order_ID SMALLSERIAL,
+        Order_ID INT NOT NULL PRIMARY KEY,
         Date_Time timestamp NOT NULL,
         Branch_ID INT NOT NULL,
-        Total_price FLOAT(2) NOT NULL,
-        PRIMARY KEY (Order_ID, Branch_ID)
+        Total_Price FLOAT(2) NOT NULL
+        
     );
     CREATE TABLE IF NOT EXISTS Branches (
         Branch_ID SMALLSERIAL PRIMARY KEY,
@@ -20,35 +20,38 @@ def create_tables():
         UNIQUE (Branch)
     );
     CREATE TABLE IF NOT EXISTS Items_Ordered (
-        Order_ID SMALLSERIAL,
+        Order_ID INT NOT NULL,
         Item_ID INT NOT NULL,
-        PRIMARY KEY (Order_ID, Item_ID)
+        Quantity INT NOT NULL
     );
     """
     return sql
 
+
 def insert_column_values():    
-            from orders_test import products123, price_for_product, run_db, Branchess
-            for prices, item in enumerate(products123):
-                price = price_for_product[prices]
-                price = float(price)
-
-                sql = F"""
-                INSERT INTO Item 
-                VALUES (
-                DEFAULT, '{item}', {price}
-                )
-                ON CONFLICT DO NOTHING
-                """
-                run_db(sql)
-
-            for Branch in Branchess:
-                sql = f"""
-                INSERT INTO Branches(
-                Branch_ID, Branch
-                )
-                VALUES
-                (DEFAULT, '{Branch}')
-                ON CONFLICT DO NOTHING
+    from orders_test import products123, price_for_product, run_db, Branchess, current_branches, items
+    for prices, item in enumerate(products123):
+        price = price_for_product[prices]
+        price = float(price)
+        
+        if item not in items:    
+            sql = f"""
+            INSERT INTO Items
+            VALUES (
+            DEFAULT, '{item}', {price}
+            )
+            ON CONFLICT DO NOTHING
             """
-                run_db(sql)
+            run_db(sql)
+            items.append(item)
+    
+    for Branch in Branchess:
+        if Branch not in current_branches:    
+            sql = f"""
+            INSERT INTO Branches(
+            Branch_ID, Branch)
+            VALUES
+            (DEFAULT, '{Branch}')
+            """
+            run_db(sql)
+            current_branches.append(Branch)
--- orders_test.py
+++ orders_test.py
@@ -1,20 +1,25 @@
 import csv
-from operator import index
 import os
 import psycopg2
-from db1 import create_tables, insert_column_values
+from db1 import create_tables#, insert_column_values
 from dotenv import load_dotenv
 
 
-orders = []
-items_ordered = []
+order_id = 0
 
 products = []
-final_products = []
+each_order_products = []
+unique_orders = []
+
+quantities = []
+item_ids_with_quantity = []
 
 Branchess = []
+current_branches = []
 
 products123 = []
+items = []
+
 price_for_product = []
 
 load_dotenv()
@@ -61,17 +66,36 @@ def run_db_with_return(sql):
     return rows
 
 
-def load_id():
+def load_ids():
     try:
-        sql = f"SELECT Branch FROM Branches"
+        sql = "SELECT Branch FROM Branches"
         a = run_db_with_return(sql)
         for branch in a:
             if branch[0] not in Branchess:
                 Branchess.append(branch[0])
+                current_branches.append(branch[0])
+
+        sql = "SELECT Item_Name, Price FROM Items"
+        b = run_db_with_return(sql)
+        for item in b:
+            if item[0] not in products123:
+                products123.append(item[0])
+                price_for_product.append(item[1])
+                items.append(item[0])
+
+        sql = """SELECT Order_ID FROM Orders
+                 ORDER BY Order_ID DESC"""
+        global order_id
+        all_ids = run_db_with_return(sql)
+        order_id = all_ids[0][0]
+        order_id = int(order_id)
+
+        return order_id 
+
     except Exception as e:
         pass
 
-load_id()
+load_ids()
 
 
 with open("chesterfield_25-08-2021_09-00-00.csv", 'r') as chesterfield_cafe_orders:
@@ -114,45 +138,76 @@ with open("chesterfield_25-08-2021_09-00-00.csv", 'r') as chesterfield_cafe_orde
 
         if line[1] not in Branchess:
             Branchess.append(line[1])
+        
+        unique_orders.append({"Date_Time" : line[0], "Branch" : Branchess.index(line[1])+1, "Item_Name" : final_products, "Total_Price" : line[4]})
 
-        for product in final_products:
-            orders.append({"Date_Time" : line[0], "Branch" : Branchess.index(line[1])+1, "Total_Price" : line[4]})
-            items_ordered.append({"Item_Name" : (products123.index(product)+1)})
+for i in unique_orders:
+    indexes = []
+    for y in i["Item_Name"]:
+        indexes.append(products123.index(y)+1)
+    each_order_products.append(indexes)
+    
+for i in each_order_products:   
+    list = []
+    for y in i:
+        x = 0
+        quantity = 0
+        temp_number = y
+        for y in i:
+            if temp_number == i[x]:
+                quantity += 1
+            x += 1
+        list.append(quantity)     
+    quantities.append(list)
+
+for i, y in enumerate(each_order_products):
+    orders12345 = []
+    z = quantities[i]
+    for a, b in enumerate(z):
+        c = y[a]
+        orders12345.append({"id" : i+1, "item_id" : c, "quantity" : b})
+
+    item_ids_with_quantity.append([[i for n, i in enumerate(orders12345) if i not in orders12345[n + 1:]]])
 
 
 def update_db():
-    
-    for i in orders:
+    orderID = order_id + 1
+
+    for i in unique_orders:
         sql = f"""
         SET datestyle = dmy;
         INSERT INTO Orders (
         Order_ID, Date_Time, Branch_ID, Total_Price)
-        VALUES (DEFAULT, '{i['Date_Time']}',
-        {i["Branch"]}, {i['Total_Price']})
+        VALUES ({orderID}, '{i['Date_Time']}',
+        {i["Branch"]}, {i["Total_Price"]})
         """
         run_db(sql)
-        
-    for i in items_ordered:
-        sql = f"""
-        INSERT INTO Items_Ordered (
-        Order_ID, Item_ID)
-        VALUES (DEFAULT, {i['Item_Name']})
-        """
-        run_db(sql)
+        orderID += 1
+
+    orderID = order_id + 1
+    for i in item_ids_with_quantity:
+        for y in i[0]:
+            sql = f"""
+            INSERT INTO Items_Ordered (
+            Order_ID, Item_ID, Quantity)
+            VALUES ({orderID}, {y["item_id"]},
+            {y["quantity"]})
+            """
+            run_db(sql)
+        orderID += 1
 
 
 def display_all_orders():
     sql = """
-    SELECT o.Order_ID, b.Branch, to_char(o.Date_Time, 'DD-MM-YYYY HH:MI'), it.Item_Name, o.Total_Price
+    SELECT o.Order_ID, b.Branch, to_char(o.Date_Time, 'DD-MM-YYYY HH:MI'), it.Item_Name, io.Quantity, o.Total_Price
     FROM Orders o
     INNER JOIN Branches b ON b.Branch_ID = o.Branch_ID
     INNER JOIN Items_Ordered io ON o.Order_ID = io.Order_ID
-    INNER JOIN Item it ON it.Item_ID = io.Item_ID
-
+    INNER JOIN Items it ON it.Item_ID = io.Item_ID
     """
     orders = (run_db_with_return(sql))
     for order in orders:
-        print(order)
+        print({"Order_ID": order[0], "Branch": order[1], "Date and Time" : order[2], "Product Ordered" : order[3], "Quantity" : order[4], "Total Price" : order[5]})
 
 
 while True:
@@ -167,16 +222,21 @@ Enter '0' to exit application:
         if option == '1':
             sql = create_tables()
             run_db(sql)
+
         elif option == '2':
-            sql = insert_column_values()
-            run_db(sql)
+            from db1 import insert_column_values
+            insert_column_values()
+            # run_db(sql)
+
         elif option == '3':  
             update_db()
+
         elif option == '4':
             display_all_orders()
+
         elif option == '0':
             break
 
     except Exception as e:
-        print("Exception: ",e)
+        print("Exception", e)
         break
